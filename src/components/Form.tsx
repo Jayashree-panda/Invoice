1b857695875ea6875d962b7e5154365037928780
muskankhedia <muskan.khedia2000@gmail.com>
2020-09-18 15:39:48 +0530

    added styles and text-editor

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,9 @@ import AsyncSelect from "react-select/async";
 import Title from "antd/lib/typography/Title";
 import TextArea from "antd/lib/input/TextArea";
 import DatePicker from "react-datepicker";
-
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "./form.css";
 
@@ -62,6 +64,11 @@ const Form: FC<{}> = () => {
   const [quotationHeading, setQuotationHeading] = useState<string>("QUOTATION");
   const [value, setValue] = useState<string>("");
   const [startDate, setStartDate] = useState(new Date());
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (editorState: React.SetStateAction<EditorState>) => {
+    setEditorState(editorState);
+  }
 
   const handleInputChange = (newValue: string) => {
     const inputValue = newValue.replace(/\W/g, "");
@@ -154,7 +161,12 @@ const Form: FC<{}> = () => {
               <div className="document-edit-block document-layout__section">
                 <label className="document-edit-block__label">Date</label>
                 <div className="document-edit-block__content">
-                  <DatePicker selected={startDate} onChange={handleChange} />
+                  <DatePicker
+                    selected={startDate}
+                    dateFormat="dd MMM yyyy"
+                    onChange={handleChange}
+                    className="document-edit-block__input document-edit-block__datefield"
+                  />
                 </div>
               </div>
             </Col>
@@ -177,8 +189,7 @@ const Form: FC<{}> = () => {
         </Col>
         <Col span={12}>
           <div className="document-layout__section">
-            <div className="document-edit-block__content drop-down_content-lg">
-            </div>
+            <div className="document-edit-block__content drop-down_content-lg"></div>
           </div>
         </Col>
       </Row>
@@ -263,10 +274,22 @@ const Form: FC<{}> = () => {
               Terms / Method of Payment
             </label>
             <div className="document-edit-block__content">
-              <TextArea
-                value={value}
-                placeholder="Controlled autosize"
-                autoSize={{ minRows: 3, maxRows: 5 }}
+              <Editor
+                editorState={editorState}
+                toolbarOnFocus
+                toolbarClassName="document-edit-block__rich-text-editor-toolbar"
+                wrapperClassName="document-edit-block__rich-text-editor-wrapper"
+                editorClassName="document-edit-block__rich-text-editor-editor"
+                toolbar={{
+                  options: ["inline", "list", "textAlign"],
+                  inline: {
+                    options: ["bold", "italic", "underline"],
+                  },
+                  list: {
+                    options: ["unordered", "ordered"],
+                  },
+                }}
+                onEditorStateChange={onEditorStateChange}
               />
             </div>
           </div>
@@ -279,10 +302,22 @@ const Form: FC<{}> = () => {
               Additional Information
             </label>
             <div className="document-edit-block__content">
-              <TextArea
-                value={value}
-                placeholder="Controlled autosize"
-                autoSize={{ minRows: 10, maxRows: 15 }}
+              <Editor
+                editorState={editorState}
+                toolbarOnFocus
+                toolbarClassName="document-edit-block__rich-text-editor-toolbar"
+                wrapperClassName="document-edit-block__rich-text-editor-wrapper"
+                editorClassName="document-edit-block__rich-text-editor-editor"
+                toolbar={{
+                  options: ["inline", "list", "textAlign"],
+                  inline: {
+                    options: ["bold", "italic", "underline"],
+                  },
+                  list: {
+                    options: ["unordered", "ordered"],
+                  },
+                }}
+                onEditorStateChange={onEditorStateChange}
               />
             </div>
           </div>
@@ -290,47 +325,70 @@ const Form: FC<{}> = () => {
         <Col span={12}>
           <Row>
             <div className="document-layout__section">
-              <Input placeholder="Basic usage" />
-              <Input placeholder="Basic usage" />
+              <div className="document-edit-block">
+                <div className="document-edit-block__content document-edit-block__content--underline">
+                  <div className="document-edit-block--inline">
+                    <input
+                      className="document-edit-block__input document-edit-block__textfield"
+                      type="text"
+                      placeholder="Place of issue"
+                      value=""
+                    ></input>
+                    <input
+                      className="document-edit-block__input document-edit-block__textfield"
+                      type="text"
+                      placeholder="Place of issue"
+                      value=""
+                    ></input>
+                  </div>
+                </div>
+              </div>
             </div>
           </Row>
           <Row>
             <div className="document-layout__section">
               <div className="document-edit-block">
-                <label className="document-edit-block__label">
-                  Invoice Total (Incoterms&#174; 2020)
-                </label>
-                <div
-                  className="document-edit-block__layout"
-                  style={{ display: "flex", flexDirection: "row" }}
-                >
-                  <div className="document-edit-block__layout-block block--width-015">
-                    <div className="document-edit-block">
-                      <div className="document-edit-block__content">
-                        <Input placeholder="Basic usage" />
-                      </div>
+                <div className="document-edit-block__content document-edit-block__content--underline">
+                  <label className="document-edit-block__label">
+                    Invoice Total (Incoterms&#174; 2020)
+                  </label>
+                  <div className="document-edit-block--inline">
+                    <div className="block--width-015">
+                      <input
+                        className="document-edit-block__input document-edit-block__textfield"
+                        type="text"
+                        placeholder="-"
+                        value=""
+                      ></input>
                     </div>
-                  </div>
-                  <div className="document-edit-block__layout-block">
-                    <div className="document-edit-block">
-                      <div className="document-edit-block__content">
-                        <Input placeholder="Basic usage" />
-                      </div>
+                    <div>
+                      <input
+                        className="document-edit-block__input document-edit-block__textfield"
+                        type="text"
+                        placeholder="Place"
+                        value=""
+                      ></input>
                     </div>
-                  </div>
-                  <div className="document-edit-block__layout-block block--width-015">
-                    <div className="document-edit-block">
-                      <div className="document-edit-block__content">
-                        <Input placeholder="Basic usage" />
-                      </div>
+                    <div className="block--width-015">
+                      <input
+                        className="document-edit-block__input document-edit-block__textfield"
+                        type="text"
+                        placeholder="-"
+                        value=""
+                      ></input>
                     </div>
-                  </div>
-                  <div className="document-edit-block__layout-block block--width-030">
-                    <div className="document-edit-block">
-                      <div className="document-edit-block__content">
-                        <div className="document-edit-block__total">
-                          <span className="document-edit-block__total-symbol"></span>
-                          <input type="text" className="document-edit-block__input" disabled value="0.00"></input>
+                    <div className="block--width-030">
+                      <div className="document-edit-block">
+                        <div className="document-edit-block__content">
+                          <div className="document-edit-block__total">
+                            <span className="document-edit-block__total-symbol"></span>
+                            <input
+                              type="text"
+                              className="document-edit-block__input"
+                              disabled
+                              value="0.00"
+                            ></input>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -341,30 +399,89 @@ const Form: FC<{}> = () => {
           </Row>
           <Row>
             <div className="document-layout__section">
-              Place and Date of Issue
-              <Input placeholder="Basic usage" />
+              <div className="document-edit-block">
+                <div className="document-edit-block__content document-edit-block__content--underline">
+                  <label className="document-edit-block__label2">
+                    Place and Date of Issue
+                  </label>
+                  <div className="document-edit-block--inline">
+                    <input
+                      className="document-edit-block__input document-edit-block__textfield"
+                      type="text"
+                      placeholder="Place of issue"
+                      value=""
+                    ></input>
+                    <DatePicker
+                      selected={startDate}
+                      dateFormat="dd MMM yyyy"
+                      onChange={handleChange}
+                      className="document-edit-block__input document-edit-block__datefield"
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </Row>
           <Row>
             <div className="document-layout__section">
-              Signatory Company
-              <Input placeholder="Basic usage" />
+              <div className="document-edit-block">
+                <div className="document-edit-block__content document-edit-block__content--underline">
+                  <label className="document-edit-block__label2">
+                    Signatory Company
+                  </label>
+                  <div className="document-edit-block--inline">
+                    <input
+                      className="document-edit-block__input document-edit-block__textfield"
+                      type="text"
+                      placeholder="Place of issue"
+                      value=""
+                    ></input>
+                  </div>
+                </div>
+              </div>
             </div>
           </Row>
           <Row>
             <div className="document-layout__section">
-              Name of Authorized Signatory
-              <Input placeholder="Basic usage" />
+              <div className="document-edit-block">
+                <div className="document-edit-block__content document-edit-block__content--underline">
+                  <label className="document-edit-block__label2">
+                    Name of Authorized Signatory
+                  </label>
+                  <div className="document-edit-block--inline">
+                    <input
+                      className="document-edit-block__input document-edit-block__textfield"
+                      type="text"
+                      placeholder="First Name"
+                      value=""
+                    ></input>
+                    <input
+                      className="document-edit-block__input document-edit-block__textfield"
+                      type="text"
+                      placeholder="Last Name"
+                      value=""
+                    ></input>
+                  </div>
+                </div>
+              </div>
             </div>
           </Row>
           <Row>
             <div className="document-layout__section">
-              Signature
-              <TextArea
-                value={value}
-                placeholder="Controlled autosize"
-                autoSize={{ minRows: 3, maxRows: 5 }}
-              />
+              <div className="document-edit-block">
+                <div className="document-edit-block__content document-edit-block__content--underline">
+                  <label className="document-edit-block__label2">
+                    Signature
+                  </label>
+                  <div className="document-edit-block--inline">
+                    <TextArea
+                      value={value}
+                      placeholder="Controlled autosize"
+                      autoSize={{ minRows: 3, maxRows: 5 }}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </Row>
         </Col>

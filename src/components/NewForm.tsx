0ea437f24b2599ff100ca5940cacfb7dca006403
muskankhedia <muskan.khedia2000@gmail.com>
2020-09-18 11:28:34 +0530

    minor changes

--- src/components/NewForm.tsx
+++ src/components/NewForm.tsx
@@ -1,14 +1,14 @@
-import React, { FC } from 'react'
+import React, { FC } from "react";
 import "antd/dist/antd.css";
 import "./index.css";
 import { Layout } from "antd";
-import Form from './Form';
+import Form from "./Form";
 const { Content } = Layout;
 
 const NewForm: FC<{}> = () => {
   return (
     <Layout className="layout" style={{ background: "#f5f6f8" }}>
-      <Content style={{ padding: "5rem 0rem", alignSelf: "center" }}>
+      <Content style={{ margin: "12px auto 18px" }}>
         <div className="site-layout-content">
           <Form />
         </div>
@@ -17,4 +17,4 @@ const NewForm: FC<{}> = () => {
   );
 };
 
-export default NewForm
+export default NewForm;

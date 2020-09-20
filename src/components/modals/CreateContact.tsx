791437e20eab20980825d51b99bb0fe069df432b
Jayashree-panda <jayashreepanda12345@gmail.com>
2020-09-20 18:20:19 +0530

    modal updates

--- src/components/modals/CreateContact.tsx
+++ src/components/modals/CreateContact.tsx
@@ -1,6 +1,6 @@
 /** React Components */
-import React, { FC } from "react";
-import { Row, Col, Input, Modal } from "antd";
+import React, { FC,useState } from "react";
+import { Row, Col, Input, Modal, Button } from "antd";
 
 /** Custom Styles */
 import "antd/dist/antd.css";
@@ -12,15 +12,15 @@ interface CreateContactProps {
 }
 
 const CreateContact: FC<CreateContactProps> = ({ openCreate, closeCreate }) => {
-
+  const [showAddress, setShowAddress] = useState<boolean>(true);
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState<boolean>(false);
   return (
     <Modal
       title="Fill form"
-      centered
       visible={openCreate}
       onOk={() => closeCreate(true)}
       onCancel={() => closeCreate(true)}
-      width={700}
+      width={800} 
     >
       <Row>
         <Col span={6}>
@@ -65,7 +65,6 @@ const CreateContact: FC<CreateContactProps> = ({ openCreate, closeCreate }) => {
           </div>
         </Col>
       </Row>
-
       <Row>
         <Col span={6}>
           <div className="div-element-title">Email Address</div>
@@ -80,108 +79,180 @@ const CreateContact: FC<CreateContactProps> = ({ openCreate, closeCreate }) => {
           </div>
         </Col>
       </Row>
-
-      <Row>
-        <Col span={6}>
-          <div className="div-element-title">Street Address</div>
-        </Col>
-
-        <Col span={18}>
-          <div className="div-element-modal">
-            <Input
-              size="large"
-              placeholder="Address"
-              className="input-element-modal"
-            />
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={6}>
-          <div className="div-element-title"></div>
-        </Col>
-
-        <Col span={18}>
-          <div className="div-element-modal">
-            <Input
-              size="large"
-              placeholder=""
-              className="input-element-modal"
-            />
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={6}>
-          <div className="div-element-title"></div>
-        </Col>
-        <Col span={18}>
-          <div className="div-element-modal">
-            <Input
-              size="large"
-              placeholder="City"
-              className="input-element-modal"
-            />
-          </div>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col span={6}>
-          <div className="div-element-title"></div>
-        </Col>
-
-        <Col span={18}>
+      <div className ="buttons">
+        <Row>
+          <Col span={12}>
+            <Button
+              className="addressButton"
+              onClick={() => {
+                setShowAdditionalInfo(false);
+                setShowAddress(true);
+              }}
+            >
+              Address
+            </Button>
+          </Col>
+          <Col span={12}>
+            <Button
+              className="additionalInfoButton"
+              onClick={() => {
+                setShowAddress(false);
+                setShowAdditionalInfo(true);
+              }}
+            >
+              Additional fields
+            </Button>
+          </Col>
+        </Row>
+      </div>
+      {showAddress && (
+        <div>
           <Row>
-            <Col span={9}>
-              <div className="div-element-modal">
-                <Input
-                  size="large"
-                  placeholder="Postal/ZIP Code"
-                  className="input-element-modal"
-                />
-              </div>
+            <Col span={6}>
+              <div className="div-element-title">Street Address</div>
             </Col>
-            <Col span={9}>
+
+            <Col span={18}>
               <div className="div-element-modal">
                 <Input
                   size="large"
-                  placeholder="Country"
+                  placeholder="Address"
                   className="input-element-modal"
                 />
               </div>
             </Col>
           </Row>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col span={6}>
-          <div className="div-element-title">Contact Info:</div>
-        </Col>
-        <Col span={18}>
           <Row>
-            <Col span={9}>
-              <div className="div-element-modal">
-                <Input
-                  size="large"
-                  placeholder="Telephone"
-                  className="input-element-modal"
-                />
-              </div>
+            <Col span={6}>
+              <div className="div-element-title"></div>
             </Col>
-            <Col span={9}>
+            <Col span={18}>
               <div className="div-element-modal">
                 <Input
                   size="large"
-                  placeholder="fax"
+                  placeholder=""
                   className="input-element-modal"
                 />
               </div>
             </Col>
           </Row>
-        </Col>
-      </Row>
+          <Row>
+            <Col span={6}>
+              <div className="div-element-title"></div>
+            </Col>
+            <Col span={18}>
+              <div className="div-element-modal">
+                <Input
+                  size="large"
+                  placeholder="City"
+                  className="input-element-modal"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              <div className="div-element-title"></div>
+            </Col>
+            <Col span={18}>
+              <Row>
+                <Col span={9}>
+                  <div className="div-element-modal">
+                    <Input
+                      size="large"
+                      placeholder="Postal/ZIP Code"
+                      className="input-element-modal"
+                    />
+                  </div>
+                </Col>
+                <Col span={9}>
+                  <div className="div-element-modal">
+                    <Input
+                      size="large"
+                      placeholder="Country"
+                      className="input-element-modal"
+                    />
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              <div className="div-element-title">Contact Info:</div>
+            </Col>
+            <Col span={18}>
+              <Row>
+                <Col span={9}>
+                  <div className="div-element-modal">
+                    <Input
+                      size="large"
+                      placeholder="Telephone"
+                      className="input-element-modal"
+                    />
+                  </div>
+                </Col>
+                <Col span={9}>
+                  <div className="div-element-modal">
+                    <Input
+                      size="large"
+                      placeholder="fax"
+                      className="input-element-modal"
+                    />
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </div>
+      )}
+      {showAdditionalInfo && (
+        <div>
+          <Row>
+            <Col span={6}>
+              <div className="div-element-title">Additional fields</div>
+            </Col>
+
+            <Col span={18}>
+              <div className="div-element-modal">
+                <Input
+                  size="large"
+                  placeholder="Additional field 1"
+                  className="input-element-modal"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              <div className="div-element-title"></div>
+            </Col>
+            <Col span={18}>
+              <div className="div-element-modal">
+                <Input
+                  size="large"
+                  placeholder="Additional field 2"
+                  className="input-element-modal"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              <div className="div-element-title"></div>
+            </Col>
+            <Col span={18}>
+              <div className="div-element-modal">
+                <Input
+                  size="large"
+                  placeholder="Additional field 3"
+                  className="input-element-modal"
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      )}
     </Modal>
   );
 };
